c7c92a417370589616c4cdc0d60d2e35a89aed81
Stijn Vergauwen <stijnvergauwen@gmail.com>
2022-11-25 09:54:17 +0100

    fix file names and clean up

--- views/recipe.php
+++ views/recipe.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Css/style.css">
-    <link rel="stylesheet" href="../Css/footer.css">
+    <link rel="stylesheet" href="../Css/Base.css">
+    <link rel="stylesheet" href="../Css/Footer.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet">
@@ -16,8 +16,8 @@
     <header class="page-header">
         <p>Stijn's & Joost's Receptenboek</p>
         <nav class="header-nav">
-            <a class="nav-link" href="/Views/home.php">Home</a>
-            <a class="nav-link" href="/Views/recipe.php">Recepten</a>
+            <a class="nav-link" href="/Views/Home.php">Home</a>
+            <a class="nav-link" href="/Views/Recipe.php">Recepten</a>
         </nav>
     </header>
 
