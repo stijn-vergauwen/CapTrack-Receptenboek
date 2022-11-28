43ee2b44f7bd29dd8f2ff3e17e4c962c76bfbc18
Stijn Vergauwen <stijnvergauwen@gmail.com>
2022-11-28 14:29:35 +0100

    write down db structure

---
+++ Php/Main.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DB Tables structuur:
+// 
+// Table "recipes":
+//      "id" - int - id is gwn int
+//      "title" - varchar(40) - max title lengte
+//      "description" - text - dit kan een best lange text zijn
+//      "duration" - varchar(20) - string is makkelijk, maar mag niet te lang zijn
+//      "difficulty" - int
+//      "default_people_amount" - int - duidelijke naam is wel fijn
+//      "thumbnail" - varchar(100) - link naar img
+// 
+// Table "ingredients":
+//      "id" - int - gwn int
+//      "recipe_id" - int - recipe koppeling
+//      "title" - varchar(40) - text niet te lang
+//      "amount" - float - hoeveelheid, nodig voor verandering in hoeveelheid personen
+// 
+// Table "preparation_steps":
+//      "id" - int - gwn int
+//      "recipe_id" - int - recipe koppeling
+//      "body" - text
+//      "list_order" - int - hoeveelste stap het is
