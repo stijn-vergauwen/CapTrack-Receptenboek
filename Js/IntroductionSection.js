f21be775b377fcbd909b0a6995ad1bbb531b9aa5
Hitode <schreurder1@hotmail.com>
2022-12-09 11:38:36 +0100

    added html / css / js changes to home

---
+++ Js/IntroductionSection.js
@@ -0,0 +1,13 @@
+let introBtn1 = document.getElementById("introduction-btn1")
+let introBtn2 = document.getElementById("introduction-btn1")
+
+let introDescriptionBlock = document.getElementById(intro-description-block)
+let introDescriptionBlock = document.getElementById(intro-description-block)
+let intro-video-block2 = document.getElementById(intro-description-block)
+
+introBtn1.addEventListener("click", expandIntro)
+introBtn2.addEventListener("click", expandIntro)
+
+function expandIntro {
+
+}
