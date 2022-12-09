e4ea33b61e73d81e8c10e213f1882893cf60272b
Hitode <schreurder1@hotmail.com>
2022-12-09 11:42:53 +0100

    js changes

---
+++ Js/IntroductionSection.js
@@ -0,0 +1,14 @@
+let introBtn1 = document.getElementById("introduction-btn1")
+let introBtn2 = document.getElementById("introduction-btn1")
+
+let introDescriptionBlock = document.getElementById(intro-description-block)
+let introOrganicBlock = document.getElementById(intro-description-block)
+let introVideoBlock2 = document.getElementById(intro-video-block2)
+let introVideoBlock1 = document.getElementById(intro-video-block1)
+
+introBtn1.addEventListener("click", expandIntro)
+introBtn2.addEventListener("click", expandIntro)
+
+function expandIntro {
+
+}
